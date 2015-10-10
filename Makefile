6f97a96489f7ccffb87e21edcec10e57e29418c2
István Antal <istvan.m.antal@gmail.com>
2015-10-10 16:31:09 +0100

    Added Doctrine migrations and fixed permissions for cache and logs folder

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 build:
-
+	php app/console cache:clear
+	php app/console doctrine:migrations:migrate --no-interaction
 system:
 	sudo puppet apply --modulepath=./system system.pp
 	
--- bin/install-ubuntu.sh
+++ bin/install-ubuntu.sh
@@ -8,5 +8,9 @@ sudo apt-get install --assume-yes puppet git make
 ssh-keyscan github.com >> ~/.ssh/known_hosts
 git clone https://github.com/istvan-antal/content-creator.git
 cd content-creator
+mkdir app/cache
+mkdir app/logs
+chmod 0777 app/cache
+chmod 0777 app/logs
 make system
 make
